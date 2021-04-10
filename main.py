ad8dda776fc200bae0183293cb03c3c0e1eb54c8
theostrich <79783371+theostrich@users.noreply.github.com>
2021-04-10 02:00:25 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,82 +1,263 @@
+from pyrogram import Client,filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from secrets import token_urlsafe
+import boto3
+import pathlib
+import time
+import database
 import os
-from Crypto.Cipher import AES
-import base64
-import logging
 
-from telegram import ParseMode, InlineKeyboardMarkup, InlineKeyboardButton, Update
-from telegram.ext import (
-    MessageHandler,
-    Updater,
-    CommandHandler,
-    CallbackQueryHandler,
-    Filters,
-    CallbackContext,
-)
+ostrich = Client("bot",
+        bot_token="",
+       api_id= ,
+        api_hash="")
 
-# Enable logging
-from telegram.utils import helpers
+bucketName = '' # Your github Bucket Name
 
-logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
-)
+@ostrich.on_message(filters.command(["start"]))
+async def start(client, message):
+    try:
+        await message.reply_text(
+        text=f"**Hello {message.chat.first_name} 👋 !"
+             "\n\nFeeling slow download speed? Don't worry, I can generate high speed download links for you. "
+             "\n\nCheck help to find out more about how to use me.**",
+        disable_web_page_preview=True,
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton("HELP", callback_data="getHelp"),
+                ]
+            ]
+        ),
+        reply_to_message_id=message.message_id
+    )
+    except:
+        await message.reply_text(
+            text=f"**Hi 👋 !"
+                 "\n\nFeeling slow download speed? Don't worry, I can generate high speed download links for you."
+                 "\n\nCheck help to find out more about how to use me.**",
+            disable_web_page_preview=True,
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton("HELP", callback_data="getHelp"),
+                    ]
+                ]
+            ),
+            reply_to_message_id=message.message_id
+        )
+    database.scrape(message)
 
-logger = logging.getLogger(__name__)
+@ostrich.on_message(filters.command(["help"]))
+async def assist(client, message):
+    await message.reply_text(
+        text=f"**Hi {message.chat.first_name}."
+                 "\nHere is a detailed guide to use me."
+                 "\n\nYou might have faced slow download speed while downloading telegram files."
+                 "\n\nForward me any telegram file and I will generate you a high speed download link."
+                 "\n\nFor further information and guidance contact my developers at my support group.**",
+                  disable_web_page_preview=True,
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton("SUPPORT GROUP", url="https://t.me/ostrichdiscussion"),
+                ]
+            ]
+        ),
+        reply_to_message_id=message.message_id
+    )
 
 
-def start(update, context):
-    usr_cmd = update.message.text.split("_")[-1]
-    if usr_cmd == "/start":
-        context.bot.sendMessage(chat_id=update.message.chat.id,
-                                text='Hi, I am Graken. I can store all your files in a safe ship.')
-    else:
+@ostrich.on_message(filters.private & (filters.document | filters.video))
+async def download(client, message):
+    media = message
+    keyboard = InlineKeyboardMarkup([
+        [InlineKeyboardButton(text="CONTINUE", callback_data="download")],
+        [InlineKeyboardButton(text="CANCEL", callback_data="close")]
+    ])
+    await message.reply_text("**Are you sure you want to continue?**", quote=True, reply_markup=keyboard)
+
+async def graken(client, message):
+      s3_resource = boto3.resource('s3')
+      media = message.reply_to_message
+      msg = await client.send_message(chat_id=message.chat.id, text='**Downloading your file to my server...**',reply_markup=InlineKeyboardMarkup([
+                                        [InlineKeyboardButton(text="Check Progress", callback_data="progress_msg")]
+                                    ]))
+      c_time = time.time()
+      try:
+        download_location = await client.download_media(message = media,progress=progress_func,
+        progress_args=(
+            "**Downloading your file to server...**",
+            msg,
+            c_time))
+      except:
+          msg.edit_text('Retrying Download, Please wait')
+          download_location = await client.download_media(message=media)
+      filename = pathlib.Path(download_location).stem
+      extension = pathlib.Path(download_location).suffix
+      toname = key('downloads') + filename[0:5] + extension
+
+      await msg.edit_text(text='Getting you a fast URL...')
+
+      s3_resource.Object('grakener', 'public/'+toname).upload_file(
+           Filename=download_location)
+
+      await msg.edit_text(text = f'**Click this to download your file:**\n\nhttps://{bucketName}.s3.us-east-2.amazonaws.com/public/{toname}',
+                          reply_markup=InlineKeyboardMarkup([
+                              [InlineKeyboardButton(text="OPEN IN BROWSER", url =f'https://{bucketName}.s3.us-east-2.amazonaws.com/public/{toname}')]
+                          ])
+                          ,disable_web_page_preview=True)
+
+      os.remove(download_location)
+
+db = {}
+
+def key(url: str, nbytes: int=5) -> str:
+        ext = token_urlsafe(nbytes=nbytes)
+        if ext in db:
+          return shorten(url, nbytes=nbytes)
+        else:
+          db.update({ext: url})
+          return ext
+
+@ostrich.on_message(filters.command(["about"]))
+async def aboutTheBot(client, message):
+    """Log Errors caused by Updates."""
+
+    keyboard = [
+        [
+            InlineKeyboardButton("➰Channel",
+                                          url="t.me/theostrich"),
+            InlineKeyboardButton("👥Support Group", url="t.me/ostrichdiscussion"),
+        ],
+        [InlineKeyboardButton("🔖Add Me In Group", url="https://t.me/iconrailsBot?startgroup=new")],
+    ]
+
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    await message.reply_text("<b>Hello! I am Graken.</b>"
+                              "\nI can generate high speed download links."
+                              "\n\n<b>About Me :</b>"
+                              "\n\n  - <b>Name</b>        : Graken"
+                              "\n\n  - <b>Creator</b>      : @theostrich"
+                              "\n\n  - <b>Language</b>  : Python 3"
+                              "\n\n  - <b>Library</b>       : <a href=\"https://docs.pyrogram.org/\">Pyrogram</a>"
+                              "\n\nIf you enjoy using me and want to help me survive, do donate with the /donate command - my creator will be very grateful! Doesn't have to be much - every little helps! Thanks for reading :)",
+                             reply_markup=reply_markup, disable_web_page_preview=True)
+
+@ostrich.on_message(filters.command(["donate"]))
+async def donate(client, message):
+    keyboard = [
+        [
+            InlineKeyboardButton("Contribute",
+                                          url="https://github.com/theostrich"),
+            InlineKeyboardButton("Paypal Us",url="https://paypal.me/donateostrich"),
+        ],
+    ]
+
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    await message.reply_text("Thank you for your wish to contribute. I hope you enjoyed using our services. Make a small donation/contribute to let this project alive." , reply_markup=reply_markup)
+
+@ostrich.on_callback_query()
+async def cb_handler(client, query):
+    if query.data == "download":
+        await query.answer()
+        await query.message.delete()
+        await graken(client, query.message)
+    elif query.data == "progress_msg":
+
         try:
-            file_d = usr_cmd
-            key = 'I am inevitable.'
-            iv = '1NfaItAqnACMk45O'
-            decryption_suite = AES.new(key, AES.MODE_CFB, iv)
-            file_id = decryption_suite.decrypt(base64.b64decode(file_d))
-            sendFile = context.bot.forward_message(chat_id=update.message.from_user.id, from_chat_id="",
-                                                   message_id=int(file_id))
+            msg = "Progress Details...\n\nCompleted : {current}\nTotal Size : {total}\nSpeed : {speed}\nProgress : {progress:.2f}%\nETA: {eta}"
+            await query.answer(
+                msg.format(
+                    **PRGRS[f"{query.message.chat.id}_{query.message.message_id}"]
+                ),
+                show_alert=True
+            )
+
         except:
-            context.bot.sendMessage(chat_id=update.message.chat.id, text='File Not Found')
+            await query.answer(
+                "Processing your file...",
+                show_alert=True
+            )
 
 
-def graken(update, context):
-    if update.message.chat.type == "private":
-        editable = update.message.reply_text("Please wait ...")
-        try:
-            forwarded = context.bot.copy_message(chat_id='',
-                                                 from_chat_id=update.message.chat.id,
-                                                 message_id=update.message.message_id)
-
-            context.bot.copy_message(chat_id='',
-                                     from_chat_id=update.message.chat.id,
-                                     message_id=update.message.message_id)
-            file_er_id = forwarded.message_id
-            key = '7I am inevitable.7'
-            iv = '71NfaItAqnACMk45O7'
-            enc_s = AES.new(key, AES.MODE_CFB, iv)
-            cipher_text = enc_s.encrypt(str(file_er_id))
-            encoded_cipher_text = base64.b64encode(cipher_text)
-            sharelink = f"https://telegram.dog/iconrails_bot?start=graken_{(str(encoded_cipher_text))[2:-1]}"
-            context.bot.sendMessage(chat_id='@grakenBot', text=f"Done\n{forwarded.message_id}",
-                                    reply_to_message_id=forwarded.message_id)
-            context.bot.editMessageText(f"File stored safely in  a safest place, get it anytime using :\n\n{sharelink}",
-                                        chat_id=update.message.chat_id,
-                                        message_id=editable.message_id)
-        except:
-            context.bot.sendMessage(chat_id=update.message.chat.id, text='Something went wrong')
+    elif query.data == "close":
+        await query.message.delete()
+        await query.answer(
+        "Process Cancelled..."
+    )
+    elif query.data == "getHelp":
+        await query.answer()
+        await query.message.edit_text(
+            text=f"**Hi {query.message.chat.first_name}."
+                 "\nHere is a detailed guide to use me."
+                 "\n\nYou might have faced slow download speed while downloading telegram files."
+                 "\n\nForward me any telegram file and I will generate you a high speed download link."
+                 "\n\nFor further information and guidance contact my developers at my support group.**",
+                  reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton("SUPPORT GROUP", url="https://t.me/ostrichdiscussion"),
+                ]
+            ]
+        ),
+        disable_web_page_preview=True
+        )
+        return
 
 
-def main():
-    TOKEN = ""
-    updater = Updater(TOKEN)
-    # Get the dispatcher to register handlers
-    dispatcher = updater.dispatcher
-    dispatcher.add_handler(CommandHandler("start", start))
-    dispatcher.add_handler(MessageHandler((Filters.audio | Filters.document | Filters.video), graken))
-    updater.start_polling()
+PRGRS = {}
+
+async def progress_func(
+        current,
+        total,
+        ud_type,
+        message,
+        start
+):
+    now = time.time()
+    diff = now - start
+    if round(diff % 5.00) == 0 or current == total:
+        percentage = current * 100 / total
+        speed = current / diff
+        af = total / speed
+        elapsed_time = round(af) * 1000
+        time_to_completion = round((total - current) / speed) * 1000
+        estimated_total_time = elapsed_time + time_to_completion
+        eta =  TimeFormatter(milliseconds=time_to_completion)
+
+        PRGRS[f"{message.chat.id}_{message.message_id}"] = {
+            "current": humanbytes(current),
+            "total": humanbytes(total),
+            "speed": humanbytes(speed),
+            "progress": percentage,
+            "eta": eta
+        }
 
 
-if __name__ == "__main__":
-    main()
+def humanbytes(size):
+    if not size:
+        return ""
+    power = 2 ** 10
+    n = 0
+    Dic_powerN = {0: ' ', 1: 'K', 2: 'M', 3: 'G', 4: 'T'}
+    while size > power:
+        size /= power
+        n += 1
+    return str(round(size, 2)) + " " + Dic_powerN[n] + 'B'
+
+
+def TimeFormatter(milliseconds: int) -> str:
+    seconds, milliseconds = divmod(int(milliseconds), 1000)
+    minutes, seconds = divmod(seconds, 60)
+    hours, minutes = divmod(minutes, 60)
+    days, hours = divmod(hours, 24)
+    tmp = ((str(days) + "d, ") if days else "") + \
+          ((str(hours) + "h, ") if hours else "") + \
+          ((str(minutes) + "m, ") if minutes else "") + \
+          ((str(seconds) + "s, ") if seconds else "") + \
+          ((str(milliseconds) + "ms, ") if milliseconds else "")
+    return tmp[:-2]
+
+ostrich.run()
