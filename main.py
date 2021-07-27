d91f526f3606bf198108ec8379177521fbad7667
RabbitFoRed <73241991+RabbitFored@users.noreply.github.com>
2021-07-27 16:33:16 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,27 @@
 from pyrogram import Client,filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from secrets import token_urlsafe
-import boto3
+from pyrogram.errors import UserNotParticipant
+import requests
+import json
+import aiohttp
 import pathlib
 import time
+import database
 import os
+import youtube_dl
 
-ostrich = Client("bot",
-        bot_token="",
-       api_id= ,
-        api_hash="")
 
-bucketName = '' # Your github Bucket Name
+ostrich = Client("theostrich",
+        bot_token=os.getenv("TOKEN"),
+       api_id=os.getenv("API_ID"),
+        api_hash=os.getenv("API_HASH"))
 
 @ostrich.on_message(filters.command(["start"]))
 async def start(client, message):
     try:
         await message.reply_text(
         text=f"**Hello {message.chat.first_name} 👋 !"
-             "\n\nFeeling slow download speed? Don't worry, I can generate high speed download links for you. "
+             "\n\nAre you feeling slow download speed? Don't worry. I can generate high-speed download links and store those files for you. "
              "\n\nCheck help to find out more about how to use me.**",
         disable_web_page_preview=True,
         reply_markup=InlineKeyboardMarkup(
@@ -33,7 +36,7 @@ async def start(client, message):
     except:
         await message.reply_text(
             text=f"**Hi 👋 !"
-                 "\n\nFeeling slow download speed? Don't worry, I can generate high speed download links for you."
+                 "\n\nAre you feeling slow download speed? Don't worry. I can generate high-speed download links and store those files for you."
                  "\n\nCheck help to find out more about how to use me.**",
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(
@@ -45,15 +48,17 @@ async def start(client, message):
             ),
             reply_to_message_id=message.message_id
         )
+    database.scrape(message)
 
 @ostrich.on_message(filters.command(["help"]))
 async def assist(client, message):
+
     await message.reply_text(
         text=f"**Hi {message.chat.first_name}."
-                 "\nHere is a detailed guide to use me."
+                 "\nHere is a detailed guide on using me."
                  "\n\nYou might have faced slow download speed while downloading telegram files."
-                 "\n\nForward me any telegram file and I will generate you a high speed download link."
-                 "\n\nFor further information and guidance contact my developers at my support group.**",
+                 "\n\nForward me any telegram file, and I will generate you a high-speed download link."
+                 "\n\nFor further information and guidance, contact my developers at my support group.**",
                   disable_web_page_preview=True,
         reply_markup=InlineKeyboardMarkup(
             [
@@ -66,17 +71,63 @@ async def assist(client, message):
     )
 
 
+@ostrich.on_message(filters.command(["utube"]))
+async def utube(client, message):
+    ydl = youtube_dl.YoutubeDL()
+    try:
+        url = message.command[1]
+        try:
+            result = ydl.extract_info(url, download=False)
+            await message.reply_text(f"**Click this to download your file:**\n\n{result['url']}",reply_markup=InlineKeyboardMarkup([
+                              [InlineKeyboardButton(text="OPEN IN BROWSER", url =f'{result["url"]}')]
+                          ])
+                          ,disable_web_page_preview=True, parse_mode=None)
+        except:
+            with youtube_dl.YoutubeDL(dict(forceurl=True)) as ydl:
+                r = ydl.extract_info(url, download=False)
+                media_url = r['formats'][-1]['url']
+                print(media_url)
+                await message.reply_text(f"Click this to download your file:\n\n{media_url}",
+                                   reply_markup=InlineKeyboardMarkup([
+                                       [InlineKeyboardButton(text="OPEN IN BROWSER", url=f'{media_url}')]
+                                   ])
+                                   , disable_web_page_preview=True, parse_mode=None)
+    except:
+        await message.reply_text('**Provide me some URL to get direct link\n\nEg:**\n\t\t`/utube https://www.youtube.com/watch?v=h6fcK_fRYaI`')
+
+
 @ostrich.on_message(filters.private & (filters.document | filters.video))
 async def download(client, message):
     media = message
-    keyboard = InlineKeyboardMarkup([
-        [InlineKeyboardButton(text="CONTINUE", callback_data="download")],
-        [InlineKeyboardButton(text="CANCEL", callback_data="close")]
-    ])
-    await message.reply_text("**Are you sure you want to continue?**", quote=True, reply_markup=keyboard)
+    filetype = media.document or media.video
 
-async def graken(client, message):
-      s3_resource = boto3.resource('s3')
+    freelimit = 500000000
+    filesize = filetype.file_size
+    if filesize < freelimit:
+        keyboard = InlineKeyboardMarkup([
+            [InlineKeyboardButton(text="CONTINUE", callback_data="download")],
+            [InlineKeyboardButton(text="CANCEL", callback_data="close")]
+                    ])
+        await message.reply_text("**Are you sure you want to continue?**", quote=True, reply_markup=keyboard)
+    else:
+        try:
+            user = await client.get_chat_member('theostrich', message.chat.id)
+            keyboard = InlineKeyboardMarkup([
+                [InlineKeyboardButton(text="CONTINUE", callback_data="download")],
+                [InlineKeyboardButton(text="CANCEL", callback_data="close")]
+            ])
+            await message.reply_text("**Are you sure you want to continue?**", quote=True, reply_markup=keyboard)
+        except UserNotParticipant:
+            await message.reply_text(
+                text="**Due to limited resource, files greater than 500mb require channel membership.**",
+                reply_markup=InlineKeyboardMarkup([
+                    [InlineKeyboardButton(text="Join theostrich", url=f"https://t.me/theostrich")]
+              ])
+            )
+
+
+async def anonfiles(client, message):
+
       media = message.reply_to_message
       msg = await client.send_message(chat_id=message.chat.id, text='**Downloading your file to my server...**',reply_markup=InlineKeyboardMarkup([
                                         [InlineKeyboardButton(text="Check Progress", callback_data="progress_msg")]
@@ -91,32 +142,28 @@ async def graken(client, message):
       except:
           msg.edit_text('Retrying Download, Please wait')
           download_location = await client.download_media(message=media)
-      filename = pathlib.Path(download_location).stem
-      extension = pathlib.Path(download_location).suffix
-      toname = key('downloads') + filename[0:5] + extension
 
       await msg.edit_text(text='Getting you a fast URL...')
-
-      s3_resource.Object('grakener', 'public/'+toname).upload_file(
-           Filename=download_location)
-
-      await msg.edit_text(text = f'**Click this to download your file:**\n\nhttps://{bucketName}.s3.us-east-2.amazonaws.com/public/{toname}',
+      try:
+        async with aiohttp.ClientSession() as session:
+            files = {
+                'file': open(download_location, 'rb')
+            }
+            request = await session.post("https://api.anonfiles.com/upload", data=files)
+      except:
+          msg.edit_text(text='Something went wrong. Contact my developers @theostrich')
+      json_response = await request.json()
+      download_url = json_response["data"]["file"]["url"]["full"]
+      await msg.edit_text(text = f'**Click this to download your file:**\n\n{download_url}',
                           reply_markup=InlineKeyboardMarkup([
-                              [InlineKeyboardButton(text="OPEN IN BROWSER", url =f'https://{bucketName}.s3.us-east-2.amazonaws.com/public/{toname}')]
+                              [InlineKeyboardButton(text="OPEN IN BROWSER", url =f'{download_url}')]
                           ])
                           ,disable_web_page_preview=True)
 
       os.remove(download_location)
 
-db = {}
 
-def key(url: str, nbytes: int=5) -> str:
-        ext = token_urlsafe(nbytes=nbytes)
-        if ext in db:
-          return shorten(url, nbytes=nbytes)
-        else:
-          db.update({ext: url})
-          return ext
+
 
 @ostrich.on_message(filters.command(["about"]))
 async def aboutTheBot(client, message):
@@ -134,13 +181,13 @@ async def aboutTheBot(client, message):
     reply_markup = InlineKeyboardMarkup(keyboard)
 
     await message.reply_text("<b>Hello! I am Graken.</b>"
-                              "\nI can generate high speed download links."
+                              "\nI can generate high-speed download links."
                               "\n\n<b>About Me :</b>"
                               "\n\n  - <b>Name</b>        : Graken"
                               "\n\n  - <b>Creator</b>      : @theostrich"
                               "\n\n  - <b>Language</b>  : Python 3"
                               "\n\n  - <b>Library</b>       : <a href=\"https://docs.pyrogram.org/\">Pyrogram</a>"
-                              "\n\nIf you enjoy using me and want to help me survive, do donate with the /donate command - my creator will be very grateful! Doesn't have to be much - every little helps! Thanks for reading :)",
+                              "\n\nIf you enjoy using me and want to help me survive, contribute  my developers with the /donate command - my creator will be very grateful! It doesn't have to be much - every little would help us! Thanks for reading :)",
                              reply_markup=reply_markup, disable_web_page_preview=True)
 
 @ostrich.on_message(filters.command(["donate"]))
@@ -161,7 +208,7 @@ async def cb_handler(client, query):
     if query.data == "download":
         await query.answer()
         await query.message.delete()
-        await graken(client, query.message)
+        await anonfiles(client, query.message)
     elif query.data == "progress_msg":
 
         try:
@@ -189,10 +236,10 @@ async def cb_handler(client, query):
         await query.answer()
         await query.message.edit_text(
             text=f"**Hi {query.message.chat.first_name}."
-                 "\nHere is a detailed guide to use me."
+                 "\nHere is a detailed guide on using me."
                  "\n\nYou might have faced slow download speed while downloading telegram files."
-                 "\n\nForward me any telegram file and I will generate you a high speed download link."
-                 "\n\nFor further information and guidance contact my developers at my support group.**",
+                 "\n\nForward me any telegram file, and I will generate you a high-speed download link."
+                 "\n\nFor further information and guidance, contact my developers at my support group.**",
                   reply_markup=InlineKeyboardMarkup(
             [
                 [
@@ -224,6 +271,8 @@ async def progress_func(
         time_to_completion = round((total - current) / speed) * 1000
         estimated_total_time = elapsed_time + time_to_completion
         eta =  TimeFormatter(milliseconds=time_to_completion)
+        elapsed_time = TimeFormatter(milliseconds=elapsed_time)
+        estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)
 
         PRGRS[f"{message.chat.id}_{message.message_id}"] = {
             "current": humanbytes(current),
